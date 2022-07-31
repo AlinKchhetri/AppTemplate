db961fc12883c9bf20502ed2479bb9f36d50047b
AlinKchhetri <kcalan672@gmail.com>
2022-07-31 17:04:39 +0545

    Initial Template

---
+++ screens/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home"
+import Search from "./Search"
+import Reels from "./Reels";
+import Shop from "./Shop";
+import Profile from "./Profile";
+
+export {
+    Home,
+    Search,
+    Reels,
+    Shop,
+    Profile
+};
